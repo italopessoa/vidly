ef3b75b5bfc9c6050e78596626ad9da595fd6201
Italo Pessoa <italo.pessoa@hotmail.com>
2017-01-11 23:55:35 -0300

    add CustomerDto

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MemberShipTypeId { get; set; }
+
+        [Display]
+        public DateTime? Birthdate { get; set; }
+    }
+}
