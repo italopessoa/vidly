b7114638a65f55d3e8a1977d5ff1a40d99ebd1ee
italopessoa <italo.pessoa@hotmail.com>
2017-01-13 13:57:19 -0300

    add drive license to ApplicationUser migration

--- Vidly/Migrations/Configuration.cs
+++ Vidly/Migrations/Configuration.cs
@@ -4,12 +4,15 @@ namespace Vidly.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-
+    //http://stackoverflow.com/questions/13510341/entity-framework-5-migrations-setting-up-an-initial-migration-and-single-seed-o
     internal sealed class Configuration : DbMigrationsConfiguration<Vidly.Models.ApplicationDbContext>
     {
+        public readonly bool _pendingMigrations;
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            //var migrator = new DbMigrator(this);
+            //_pendingMigrations = migrator.GetPendingMigrations().Any();
         }
 
         protected override void Seed(Vidly.Models.ApplicationDbContext context)
@@ -26,6 +29,8 @@ namespace Vidly.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            //if (!_pendingMigrations) return;
         }
     }
 }
