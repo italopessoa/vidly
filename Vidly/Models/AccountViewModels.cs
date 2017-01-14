d9288dbdec550201863db4e2a55656601dcb3033
Italo Pessoa <italo.pessoa@hotmail.com>
2017-01-13 22:46:09 -0300

    classes refactored

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -3,22 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    //public class ExternalLoginConfirmationViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DrivingLicense { get; set; }
+    //    [Required]
+    //    [Display(Name = "Driving License")]
+    //    public string DrivingLicense { get; set; }
 
-        [Required]
-        [StringLength(50, ErrorMessage = "Phone number is too long. Maximum 50 numbers.")]
-        //[Range(0,9)]
-        [Display(Name = "Phone")]
-        public string Phone { get; set; }
-    }
+    //    [Required]
+    //    [StringLength(50, ErrorMessage = "Phone number is too long. Maximum 50 numbers.")]
+    //    //[Range(0,9)]
+    //    [Display(Name = "Phone")]
+    //    public string Phone { get; set; }
+    //}
 
     public class ExternalLoginListViewModel
     {
@@ -72,34 +72,34 @@ namespace Vidly.Models
         public bool RememberMe { get; set; }
     }
 
-    public class RegisterViewModel
-    {
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DriveLicense { get; set; }
+    //public class RegisterViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Driving License")]
+    //    public string DriveLicense { get; set; }
 
-        [Required]
-        //[Range(0, 9)]
-        [StringLength(50,ErrorMessage="Phone number is too long. Maximum 50 numbers.")]
-        [Display(Name = "Phone")]
-        public string Phone { get; set; }
+    //    [Required]
+    //    //[Range(0, 9)]
+    //    [StringLength(50,ErrorMessage="Phone number is too long. Maximum 50 numbers.")]
+    //    [Display(Name = "Phone")]
+    //    public string Phone { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+    //    [Required]
+    //    [EmailAddress]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+    //    [Required]
+    //    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Password")]
+    //    public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-    }
+    //    [DataType(DataType.Password)]
+    //    [Display(Name = "Confirm password")]
+    //    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    //    public string ConfirmPassword { get; set; }
+    //}
 
     public class ResetPasswordViewModel
     {
