7c613eaf52ccd30f0ac26f3644c03ae9032fca2f
Italo Pessoa <italo.pessoa@hotmail.com>
2017-01-17 20:33:53 -0300

    add rental api controller usin dto. Add Rental Migration

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            using (ApplicationDbContext dbContext = new ApplicationDbContext())
+            {
+                var movies = dbContext.Movies.Where(
+                    m => newRental.MoviesId.Contains(m.Id));
+
+                var customer = dbContext.Customers.Single(
+                    c => c.Id == newRental.CustomerId);
+
+                foreach (Movie movie in movies)
+                {
+                    Rental rental = new Rental
+                    {
+                        Customer = customer,
+                        Movie = movie,
+                        DateRented = DateTime.Now
+                    };
+                    dbContext.Rentals.Add(rental);
+                }
+
+                dbContext.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+}
--- Vidly/Models/Rental.cs
+++ Vidly/Models/Rental.cs
@@ -11,11 +11,11 @@ namespace Vidly.Models
         public int Id { get; set; }
 
         [Required]
-        public int CustomerId { get; set; }
+        //public int CustomerId { get; set; }
         public Customer Customer { get; set; }
 
         [Required]
-        public int MovieId { get; set; }
+        //public int MovieId { get; set; }
         public Movie Movie { get; set; }
 
         public DateTime DateRented { get; set; }
