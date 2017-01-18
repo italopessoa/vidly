82da51c7ee49cde95c02b066e895bbea6ab954f5
Italo Pessoa <italo.pessoa@hotmail.com>
2017-01-17 21:10:40 -0300

    defensive and optmistic approach

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -16,21 +16,37 @@ namespace Vidly.Controllers.Api
         {
             using (ApplicationDbContext dbContext = new ApplicationDbContext())
             {
-                var movies = dbContext.Movies.Where(
-                    m => newRental.MoviesId.Contains(m.Id));
-
+                //if (newRental.MovieIds.Count == 0)
+                //    return BadRequest("No movie Ids have been giver.");
+                //defensive
+                
                 var customer = dbContext.Customers.Single(
                     c => c.Id == newRental.CustomerId);
 
+                //if (customer == null)
+                //    return BadRequest("Customer Id is not valid.");
+                //defensive
+
+                var movies = dbContext.Movies.Where(
+                    m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+                //if (movies.Count != newRental.MovieIds.Count)
+                //    return BadRequest("One or more MovieIds are invalid.");
+                //defensive
+
                 foreach (Movie movie in movies)
                 {
+                    if (movie.NumberAvailable == 0)
+                        return BadRequest("Movie is not available");
+
+                    movie.NumberAvailable--;
+
                     Rental rental = new Rental
                     {
                         Customer = customer,
                         Movie = movie,
                         DateRented = DateTime.Now
                     };
-                    movie.NumberAvailable--;
                     dbContext.Rentals.Add(rental);
                 }
 
